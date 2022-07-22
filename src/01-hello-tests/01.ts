05cacc6f68a116685bd6a38ee6f8f10df02ac7a9
VictorSvetailo <victorioeve@gmail.com>
2022-07-22 18:00:44 +0300

    Commit #1 tests in React

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,15 @@
+// const sentence = 'Hello my friends!'
+
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+export function mult(a: number, b: number) {
+    return a * b;
+}
+
+
+// function splitIntoWorlds(sentence: string) {
+//     return ['hello', 'my', 'friend']
+// }
+
