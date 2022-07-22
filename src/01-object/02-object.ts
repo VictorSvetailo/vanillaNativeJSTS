1e3835f8ac8d89c2c149fea2c57a26458416a8b3
VictorSvetailo <victorioeve@gmail.com>
2022-07-22 19:13:46 +0300

    Commit #2 Object Array

---
+++ src/01-object/02-object.ts
@@ -0,0 +1,58 @@
+type CityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: CityType
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    name: 'Victor',
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: 'Surganova 2',
+        city: {
+            title: 'Minsk',
+            countryTitle: 'Belarus',
+        }
+    },
+    technologies:[
+        {
+            id: 1,
+            title: 'HTML',
+        },
+        {
+            id: 2,
+            title: 'CSS',
+        },
+        {
+            id: 3,
+            title: 'REACT',
+        },
+        {
+            id: 4,
+            title: 'Node.js',
+        }
+    ]
+}
+
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
