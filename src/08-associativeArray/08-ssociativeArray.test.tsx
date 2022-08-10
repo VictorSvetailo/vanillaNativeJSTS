0339ef4565ce383b40685701354ffb06dd636209
VictorSvetailo <victorioeve@gmail.com>
2022-08-10 18:51:44 +0300

    Commit #8 native JS Associative array

---
+++ src/08-associativeArray/08-ssociativeArray.test.tsx
@@ -0,0 +1,30 @@
+import {userObj} from './08-ssociativeArray';
+
+type UsersType = {
+    [key: string]: { id: number, name: string }
+}
+
+let users: UsersType
+
+beforeEach (()=>{
+    users = {
+        '101': {id: 101, name: 'Victor'},
+        '1212': {id: 1212, name: 'Dima'},
+        '3232312': {id: 3232312, name: 'Valera'},
+        '1': {id: 1, name: 'Katya'},
+    }
+})
+
+test( 'should select corresponding user', ()=>{
+    users['1'].name = 'Ekaterina'
+
+    // expect(userObj[0]).toBe('Victor')
+    expect(users['1']).toEqual({id: 1, name: 'Ekaterina'})
+    // expect(userObj[2]).toBe('Valera')
+    // expect(userObj[3]).toBe('Katya')
+})
+
+test( 'should delete corresponding user', ()=>{
+    delete users['1']
+    expect(users['1']).toBeUndefined()
+})
---
+++ src/08-associativeArray/08-ssociativeArray.tsx
@@ -0,0 +1,37 @@
+// export const userArray = ['Victor', 'Dima', 'Valera', 'Katya']
+
+export const userObj = {
+    '0': 'Victor',
+    '1': 'Dima',
+    '2': 'Valera',
+    '3': 'Katya',
+}
+
+// export type UsersType = {
+//     [key: string]: { id: number, name: string }
+// }
+//
+// export const users: UsersType = {
+//     '101': {id: 101, name: 'Victor'},
+//     '1212': {id: 1212, name: 'Dima'},
+//     '3232312': {id: 3232312, name: 'Valera'},
+//     '1': {id: 1, name: 'Katya'},
+// }
+
+// var user = {id: 100500, name: 'Igor'}
+// users[user.id] = user;
+//
+// delete users[user.id]
+
+export const usersArray = [
+    {id: 101, name: 'Victor'},
+    {id: 1212, name: 'Dima'},
+    {id: 3232312, name: 'Valera'},
+    {id: 1, name: 'Katya'},
+]
+
+// var usersArray = usersArray.filter(u=> u.id !== user.id)
+
+// var usersCopy = [...usersArray.filter(), user]
+// usersArray.push(user)
+
